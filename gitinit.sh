00ee78d1fef62fe26b302fe91697b5e4d2346f86
AJB1000 <venteslbc1000@gmail.com>
2025-10-31 11:32:16 +0100

    Mise à jour ven. 31 oct. 2025 11:32:16 CET

--- gitinit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# gitinit.sh
-
-read -p "Nom du repertoire git, sans suffixe: " "dirname"
-git init
-git remote add origin https://github.com/AJB1000/$dirname.git
-git add .
-git commit -m "Déploiement initial"
-git push -u origin master
